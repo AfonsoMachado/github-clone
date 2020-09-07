03fd443ca390cee54d520b600fce7973b3f50375
Afonso Machado <afonso777@hotmail.com>
2020-09-07 00:05:58 -0300

    estrutura de rotas no app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
-  return <h1>Github Clone</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
