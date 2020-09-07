043ae479028fa25d3d2b144867a461b20f0f1fec
Afonso Machado <afonso777@hotmail.com>
2020-09-07 16:35:37 -0300

    definindo props no componente profile data

--- src/components/ProfileData/index.tsx
+++ src/components/ProfileData/index.tsx
@@ -2,7 +2,30 @@ import React from 'react';
 
 // import { Container } from './styles';
 
-const ProfileData: React.FC = () => {
+interface Props {
+  username: string;
+  name: string;
+  avatarUrl: string;
+  followers: number;
+  following: number;
+  company?: string;
+  location?: string;
+  email?: string;
+  blog?: string;
+}
+
+// Consumindo os dados passados através de Props
+const ProfileData: React.FC<Props> = ({
+  username,
+  name,
+  avatarUrl,
+  followers,
+  following,
+  company,
+  location,
+  email,
+  blog,
+}) => {
   return <div />;
 };
 
