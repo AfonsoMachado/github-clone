627a21f068564007ae72bdd6038fc531cc4d8cb9
Afonso Machado <afonso777@hotmail.com>
2020-09-07 00:15:08 -0300

    criando estrutura básica de componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import Header from './components/Header';
+import Profile from './pages/Profile';
+import Repo from './pages/Repo';
+
 function App() {
   return (
     <BrowserRouter>
+      <Header />
+
       <Routes>
-        <Route />
+        {/* Rotas da aplicação */}
+        <Route path="/" element={<Profile />} />
+        <Route path="/:username" element={<Profile />} />
+        <Route path="/:username/:reponame" element={<Repo />} />
       </Routes>
     </BrowserRouter>
+
+    // <Footer />
   );
 }
 
