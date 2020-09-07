a7539cfd723fe76074f2b651907c8eee5010787d
Afonso Machado <afonso777@hotmail.com>
2020-09-07 16:28:10 -0300

    estrutura basica da pagina de perfil

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -2,16 +2,30 @@ import React from 'react';
 
 import { Container, Main, LeftSide, RightSide } from './styles';
 
+import ProfileData from '../../components/ProfileData';
+
 const Profile: React.FC = () => {
   return (
     <Container>
       <Main>
-        <LeftSide></LeftSide>
+        <LeftSide>
+          <ProfileData
+            username={'afonsomachado'}
+            name={'Afonso Machado'}
+            avatarUrl={'https://avatars2.githubusercontent.com/u/11397955?v=4'}
+            followers={15}
+            following={32}
+            company={' '}
+            location={'Feira de Santana, Bahia, Brazil'}
+            email={'afonsosmachado@gmail.com'}
+            blog={'https://www.linkedin.com/in/afonsomachado/'}
+          />
+        </LeftSide>
 
         <RightSide></RightSide>
       </Main>
     </Container>
-  )
+  );
 };
 
 export default Profile;
