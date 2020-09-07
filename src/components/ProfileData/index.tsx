25f407a5d2de418a4d2d29e0cbdaca8241555997
Afonso Machado <afonso777@hotmail.com>
2020-09-07 16:53:15 -0300

    estrutura basica da pagina profileData

--- src/components/ProfileData/index.tsx
+++ src/components/ProfileData/index.tsx
@@ -1,6 +1,17 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import {
+  Container,
+  Flex,
+  Avatar,
+  Row,
+  PeopleIcon,
+  Column,
+  CompanyIcon,
+  LocationIcon,
+  EmailIcon,
+  BlogIcon,
+} from './styles';
 
 interface Props {
   username: string;
@@ -26,7 +37,61 @@ const ProfileData: React.FC<Props> = ({
   email,
   blog,
 }) => {
-  return <div />;
+  return (
+    <Container>
+      {/* Container que contem a foto e o nome do usuario */}
+      <Flex>
+        <Avatar src={avatarUrl} alt={username} />
+        {/* contendo o nome */}
+        <div>
+          <h1>{name}</h1>
+          <h2>{username}</h2>
+        </div>
+      </Flex>
+
+      {/* ul Contendo follower e floowing */}
+      <Row>
+        <li>
+          <PeopleIcon />
+          <b>{followers}</b>
+          <span>followers</span>
+          <span>·</span>
+        </li>
+        <li>
+          <b>{following}</b>
+          <span>following</span>
+        </li>
+      </Row>
+
+      <Column>
+        {/* aparece somente se existir uma company cadastrada */}
+        {company && (
+          <li>
+            <CompanyIcon />
+            <span>{company}</span>
+          </li>
+        )}
+        {location && (
+          <li>
+            <LocationIcon />
+            <span>{location}</span>
+          </li>
+        )}
+        {email && (
+          <li>
+            <EmailIcon />
+            <span>{email}</span>
+          </li>
+        )}
+        {blog && (
+          <li>
+            <BlogIcon />
+            <span>{blog}</span>
+          </li>
+        )}
+      </Column>
+    </Container>
+  );
 };
 
 export default ProfileData;
