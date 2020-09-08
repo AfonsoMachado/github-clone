4f3587c78549c1171170c63078720b31341d15f2
Afonso Machado <afonso777@hotmail.com>
2020-09-08 00:45:42 -0300

    estrutura inicial do pagina de respositorios

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 
-import { Container, Main, LeftSide, RightSide } from './styles';
+import { Container, Main, LeftSide, RightSide, Repos } from './styles';
 
 import ProfileData from '../../components/ProfileData';
+import RepoCard from '../../components/RepoCard';
 
 const Profile: React.FC = () => {
   return (
@@ -10,7 +11,7 @@ const Profile: React.FC = () => {
       <Main>
         <LeftSide>
           <ProfileData
-            username={'afonsomachado'}
+            username={'AfonsoMachado'}
             name={'Afonso Machado'}
             avatarUrl={'https://avatars2.githubusercontent.com/u/11397955?v=4'}
             followers={15}
@@ -22,7 +23,27 @@ const Profile: React.FC = () => {
           />
         </LeftSide>
 
-        <RightSide></RightSide>
+        <RightSide>
+          <Repos>
+            <h2>Random repos</h2>
+
+            <div>
+              {[1, 2, 3, 4, 5, 6].map((n) => (
+                <RepoCard
+                  key={n}
+                  username={'AfonsoMachado'}
+                  reponame={'calculator-react'}
+                  description={
+                    'Implementation in ReactJS of a calculator based on macOS calculator'
+                  }
+                  language={n % 3 === 0 ? 'JavaScript' : 'TypeScript'}
+                  stars={3}
+                  forks={4}
+                />
+              ))}
+            </div>
+          </Repos>
+        </RightSide>
       </Main>
     </Container>
   );
