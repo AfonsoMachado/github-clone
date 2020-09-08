c0f82233f98e60d459718e174843e9b8e8e9f3d2
Afonso Machado <afonso777@hotmail.com>
2020-09-08 01:18:55 -0300

    estilização dos icones de rpositorios

--- src/components/RepoCard/styles.ts
+++ src/components/RepoCard/styles.ts
@@ -1,13 +1,28 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { RiBookMarkLine, RiStarLine } from 'react-icons/ri';
+import { AiOutlineFork } from 'react-icons/ai';
 
 export const Container = styled.div``;
 
 export const Topside = styled.div``;
 
-export const RepoIcon = styled.div``;
+const iconCSS = css`
+  width: 16px;
+  height: 16px;
+  fill: var(--icon);
+  flex-shrink: 0;
+`;
+
+export const RepoIcon = styled(RiBookMarkLine)`
+  ${iconCSS}
+`;
 
 export const Botside = styled.div``;
 
-export const StarIcon = styled.div``;
+export const StarIcon = styled(RiStarLine)`
+  ${iconCSS}
+`;
 
-export const ForkIcon = styled.div``;
+export const ForkIcon = styled(AiOutlineFork)`
+  ${iconCSS}
+`;
