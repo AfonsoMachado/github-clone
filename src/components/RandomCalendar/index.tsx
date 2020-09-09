6feffadedf0c74f06e9b459d5e91ddc694681b9c
Afonso Machado <afonso777@hotmail.com>
2020-09-08 23:28:42 -0300

    exibindo random calendar basico na tela

--- src/components/RandomCalendar/index.tsx
+++ src/components/RandomCalendar/index.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import Heatmap from 'react-calendar-heatmap';
+import { subYears } from 'date-fns';
 
 import { Container } from './styles';
 
 const RandomCalendar: React.FC = () => {
-  return <div />;
+  // subtraindo 1 ano da data atual
+  const startDate = subYears(new Date(), 1);
+  const endDate = new Date();
+
+  return (
+    <Container>
+      <div className="wrapper">
+        {/* start date em um ano atrás */}
+        <Heatmap startDate={startDate} endDate={endDate} values={[]} />
+      </div>
+
+      <span>Random calendar (do not represent actual data)</span>
+    </Container>
+  );
 };
 
 export default RandomCalendar;
