9d49a9578412b3e3dd0eaf5bab1447b5b6754d7a
Afonso Machado <afonso777@hotmail.com>
2020-09-08 23:07:49 -0300

    inserindo duas colunas de layout para repositorio em desktop

--- src/pages/Profile/styles.ts
+++ src/pages/Profile/styles.ts
@@ -47,5 +47,10 @@ export const Repos = styled.div`
     grid-gap: 16px;
 
     grid-template-columns: 1fr;
+
+    @media (min-width: 768px) {
+      grid-template-columns: 1fr 1fr;
+      grid-auto-rows: minmax(min-content, max-content);
+    }
   }
 `;
