855b3c977201362e421dbe5b906a4fd4670dbd02
Afonso Machado <afonso777@hotmail.com>
2020-09-09 23:39:56 -0300

    estrutura basica da pagina repo

--- src/pages/Repo/index.tsx
+++ src/pages/Repo/index.tsx
@@ -1,9 +1,53 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import {
+  Container,
+  Breadcrumb,
+  RepoIcon,
+  Link,
+  Stats,
+  StarIcon,
+  ForkIcon,
+  LinkButton,
+  GithubIcon,
+} from './styles';
 
 const Repo: React.FC = () => {
-  return <div />;
+  return (
+    <Container>
+      <Breadcrumb>
+        <RepoIcon />
+        <Link className={'username'} to={'/AfonsoMachado'}>
+          AfonsoMachado
+        </Link>
+        <span>/</span>
+
+        <Link className={'reponame'} to={'/AfonsoMachado/react-calculator'}>
+          react-calculator
+        </Link>
+      </Breadcrumb>
+
+      <p>Implementation in ReactJS of a calculator based on macOS calculator</p>
+
+      <Stats>
+        <li>
+          <StarIcon />
+          <b>9</b>
+          <span>stars</span>
+        </li>
+        <li>
+          <ForkIcon />
+          <b>2</b>
+          <span>forks</span>
+        </li>
+      </Stats>
+
+      <LinkButton href={'https://github.com/AfonsoMachado/calculator-react'}>
+        <GithubIcon />
+        <span>View on GitHub</span>
+      </LinkButton>
+    </Container>
+  );
 };
 
 export default Repo;
--- src/pages/Repo/styles.ts
+++ src/pages/Repo/styles.ts
@@ -1,3 +1,19 @@
 import styled from 'styled-components';
 
 export const Container = styled.div``;
+
+export const Breadcrumb = styled.div``;
+
+export const RepoIcon = styled()``;
+
+export const Link = styled.div``;
+
+export const Stats = styled.div``;
+
+export const StarIcon = styled()``;
+
+export const ForkIcon = styled()``;
+
+export const LinkButton = styled.div``;
+
+export const GithubIcon = styled()``;
